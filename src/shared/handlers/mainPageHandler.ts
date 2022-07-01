a87924505a6b236d62e05dbd9ea5f3896277ec78
WilliamDmi <williamdemitrienko31@gmail.com>
2022-07-01 16:50:44 +0300

    added parent container

--- src/shared/handlers/mainPageHandler.ts
+++ src/shared/handlers/mainPageHandler.ts
@@ -1,4 +1,4 @@
-import PokemonListComponent from "../components/pokemonListComponent";
+import MiniPokemonComponent from "../components/miniPokemonComponent";
 import Pokemon from "../utils/pokemon";
 import PageHandler from "./pageHandler";
 
@@ -7,16 +7,18 @@ export default class MainPage extends PageHandler {
     constructor() {
         super();
         this.generateBasicPokemonList();
+        const pokemonListContainer = document.getElementById('container') as HTMLDivElement;
+        this.renderComponents(pokemonListContainer);
     }
 
     public generateBasicPokemonList(): void {
         // TODO: Decide the name of the list container for the basic Pokemon list.
         // TODO: Decide if the container is a list or a div.
-        const pokemonListContainer = document.getElementById('???') as HTMLDivElement;
+        const pokemonListContainer = document.getElementById('container') as HTMLDivElement;
         this.pokemonsStorage.forEach((pokemonData) => this.createComponent(pokemonListContainer, pokemonData));
     }
     private createComponent(parentContainer: HTMLDivElement, pokemonData: Pokemon) {
-        this.components.push(new PokemonListComponent(parentContainer, pokemonData));
+        this.components.push(new MiniPokemonComponent(parentContainer, pokemonData));
     }
 
 }
