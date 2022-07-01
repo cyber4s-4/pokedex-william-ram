eac14254cc65edc6ff95427d5132a7f58b57786c
WilliamDmi <williamdemitrienko31@gmail.com>
2022-07-01 16:51:36 +0300

    bug fix with the names

--- src/shared/handlers/pageHandler.ts
+++ src/shared/handlers/pageHandler.ts
@@ -12,23 +12,22 @@ export default class PageHandler {
     }
 
     private async fetchBasicPokemonData(): Promise<Pokemon[]> {
-        let pokemonsArray: Pokemon[] = [];
         // https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0 -> To get a list of all the pokemons
         // Maybe cache on the fly whenever an user clicks on a pokemon, if it's not existing on their local storage
         // Make a fetch and add it to the local storage?
         const spriteLink = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png';
         if (localStorage.getItem('pokedex') === null) {
-            const pokemonsList =  (await (await fetch('https://pokeapi.co/api/v2/pokemon?limit=906&offset=0')).json());
-            for(let i = 1; i < 906; i++) {
-                const pokemonImgLink = spriteLink.replace('{id}', i.toString());
+            const pokemonsList =  (await (await fetch('https://pokeapi.co/api/v2/pokemon?limit=899&offset=0')).json());
+            for(let i = 0; i < 898; i++) {
+                const pokemonImgLink = spriteLink.replace('{id}', (i+1).toString());
                 const pokemon = pokemonsList['results'][i];
-                pokemonsArray.push(new Pokemon(new BasicPokemonInfo(i.toString(), pokemon['name'], pokemonImgLink)));
+                this.pokemonsStorage.push(new Pokemon(new BasicPokemonInfo((i+1).toString(), pokemon['name'], pokemonImgLink)));
             }
-            localStorage.setItem('pokedex', JSON.stringify(pokemonsArray));
+            localStorage.setItem('pokedex', JSON.stringify(this.pokemonsStorage));
         } else {
-            pokemonsArray = (JSON.parse(localStorage.getItem('pokedex') || '{}') as Pokemon[]);
-        }
-        return pokemonsArray;
+            this.pokemonsStorage = (JSON.parse(localStorage.getItem('pokedex') || '{}') as Pokemon[]);
+        }      
+        return this.pokemonsStorage;
     }
 
     protected updateLocalStorage(): void {
@@ -45,6 +44,7 @@ export default class PageHandler {
 
     protected renderComponents(parentElement: HTMLElement): void {
         parentElement.innerHTML = '';
+        console.log(this.components);
         this.components.forEach((component) => component.render());
     }
 }
