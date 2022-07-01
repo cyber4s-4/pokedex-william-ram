e17958b416a81c6656db699437698adcd60985fc
WilliamDmi <williamdemitrienko31@gmail.com>
2022-07-01 16:48:49 +0300

    created handlers for each page

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
+import extendedInfoHandler from "./shared/handlers/extendedInfoHandler";
+import MainPage from "./shared/handlers/mainPageHandler";
+
+
 window.addEventListener('load', (e) => {
-  // fetch('https://pokeapi.co/api/v2/pokemon/ditto').then(response => response.json()).then((data) => console.log(data));
-  });
+  if (window.location.pathname === "index.html" || window.location.pathname === "/") {
+    const handler = new MainPage();  
+  }
+  else if (window.location.pathname === "pokemon.html") {
+    const urlParams = new URLSearchParams(window.location.search);
+    if(urlParams.get('id')){
+      const id:string = urlParams.get('id')!;
+      const handler = new extendedInfoHandler(id);
+
+    }
+    else window.location.href = "index.html";
+  }
+
+});
