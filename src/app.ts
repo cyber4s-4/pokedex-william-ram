8229ab1b0a56cc3badb5c36e81db77d69ca61387
WilliamDmi <williamdemitrienko31@gmail.com>
2022-07-01 17:08:19 +0300

    bug fix with the links

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,19 @@ import MainPage from "./shared/handlers/mainPageHandler";
 
 
 window.addEventListener('load', (e) => {
-  if (window.location.pathname === "index.html" || window.location.pathname === "/") {
-    const handler = new MainPage();  
+  if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
+    const handler = new MainPage();
   }
-  else if (window.location.pathname === "pokemon.html") {
+  else if (window.location.pathname === "/pokemon.html") {
     const urlParams = new URLSearchParams(window.location.search);
-    if(urlParams.get('id')){
-      const id:string = urlParams.get('id')!;
+    if (urlParams.has('id')) {
+      console.log(1);
+      const id: string = urlParams.get('id')!;
       const handler = new extendedInfoHandler(id);
-
     }
-    else window.location.href = "index.html";
+    else {
+      window.location.href = "index.html";
+    }
   }
 
 });
